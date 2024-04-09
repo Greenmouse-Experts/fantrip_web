66371cd074dcf87bfb375a185df3e4fb0f0a75f9
wills <godswillijezei@gmail.com>
2024-04-09 19:24:54 +0100

    main

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   }, []);
   return (
     <ChakraProvider>
-      {/* {screenLoading && <ScreenLoader />} */}
+      {screenLoading && <ScreenLoader />}
        <RouterProvider router={router} />
     </ChakraProvider>
   );
