b79923858ccc481b04dda42e7af73ad5153bab8c
wills <godswillijezei@gmail.com>
2024-07-29 15:59:01 +0100

    error changes

--- src/modules/users/host/setup/modal/submission-alert.tsx
+++ src/modules/users/host/setup/modal/submission-alert.tsx
@@ -12,17 +12,27 @@ const SubmissionAlert: FC<Props> = ({ close }) => {
         <FaCheckCircle className="text-[105px] text-[#9847FE] animate-bounce mx-auto" />
       </div>
       <div className="w-9/12 mx-auto text-center mt-6">
-        <p>
-          Your profile information has been sent to the admin for verification.
-          You can proceed to create a new lisitng.
+        <p>Don&apos;t Miss the Goal!</p>
+        <p className="mt-3">Hey there, superfan! 🏟️</p>
+        <p className="mt-3">
+          You&apos;re just a few steps away from scoring big with your fantrip
+          listing! ⚽ Don&apos;t leave your fellow fans hanging – complete your
+          listing and join the league of top hosts.
+        </p>
+        <p className="mt-3">
+          🏆 Finish strong and get ready to welcome fans who share your passion
+          for the game.
         </p>
       </div>
       <div className="flex justify-between items-center mt-4">
-        <div className="btn-int px-6 py-2 bg-black rounded-full" onClick={close}>
+        <div
+          className="btn-int px-6 py-2 bg-black rounded-full"
+          onClick={close}
+        >
           Close
         </div>
         <Link to={"/user/host"} className="btn-primary px-4 py-2">
-          Start a new listing
+          Finish my listing
         </Link>
       </div>
     </div>
