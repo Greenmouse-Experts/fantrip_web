48f19366c9e4fb3a82675855197ef65053081de6
wills <godswillijezei@gmail.com>
2024-11-01 16:02:19 +0100

    devs

--- src/modules/users/reservation/components/payment/index.tsx
+++ src/modules/users/reservation/components/payment/index.tsx
@@ -17,7 +17,7 @@ const PaymentButton: FC<Props> = ({ price, currency, id, checkin }) => {
     <div>
       <div className={`${price? 'absolute w-full bottom-5' : 'w-full mt-2 lg:mt-0'}`}>
         {isAfter ? (
-          <button className={`${price? 'btn-int bg-gray-400 w-full text-center py-3 !fw-600 text-lg' : 'btn-int bg-gray-400 fs-400 w-full text-center py-2 !fw-600 text-lg'}`}>
+          <button className={`${price? 'btn-int bg-gray-400 w-full text-center py-1 !fw-600 text-lg' : 'btn-int bg-gray-400 fs-400 w-full text-center py-1 px-2 !fw-600'}`}>
             Past Check-In Date
           </button>
         ) : (
--- src/modules/users/reservation/components/payment/payment-modal.tsx
+++ src/modules/users/reservation/components/payment/payment-modal.tsx
@@ -1,63 +1,76 @@
-import  { FC, useEffect } from 'react'
+import { FC, useEffect, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from './payment-element';
-import { useMutation } from '@tanstack/react-query';
-import { guestInitatePayment } from '@/services/api/booking-api';
-import { useToast } from '@chakra-ui/react';
-import PaymentFieldShimmer from '@/components/shimmers/payment-fields';
-import { STRIPE_KEY } from '@/services/constant';
+import CheckoutForm from "./payment-element";
+import { useMutation } from "@tanstack/react-query";
+import { guestInitatePayment } from "@/services/api/booking-api";
+import { useToast } from "@chakra-ui/react";
+import PaymentFieldShimmer from "@/components/shimmers/payment-fields";
+import { STRIPE_KEY } from "@/services/constant";
+import BillingAddress from "./billing-address";
 
 const stripePromise = loadStripe(`${STRIPE_KEY}`);
-interface Props{
-    id: string
+interface Props {
+  id: string;
 }
-const PaymentModal:FC<Props> = ({id}) => {
-    const {mutate, data, isPending} = useMutation({
-        mutationFn: guestInitatePayment,
-        mutationKey: ['guest-initiate-payment']
-    })
-    const toast = useToast()
-    const handleFetch = () => {
-        mutate({
-            reservation: id
-        }, {
-            onSuccess: () => {
-                // toast({
-                //   render: () => (
-                //     <div className="text-white w-[290px] text-center fw-600 syne bg-gradient rounded p-3">
-                //       {data.message}
-                //     </div>
-                //   ),
-                //   position: "top",
-                // });
-            },
-            onError: (err:any) => {
-                toast({
-                    title: err.response.data.message,
-                    isClosable: true,
-                    position: "top",
-                    status: "error",
-                  });
-            }
-        })
-    }
-    useEffect(() => {
-        handleFetch()
-    },[])
+const PaymentModal: FC<Props> = ({ id }) => {
+  const { mutate, data, isPending } = useMutation({
+    mutationFn: guestInitatePayment,
+    mutationKey: ["guest-initiate-payment"],
+  });
+  const toast = useToast();
+  const handleFetch = () => {
+    mutate(
+      {
+        reservation: id,
+      },
+      {
+        onSuccess: () => {
+          // toast({
+          //   render: () => (
+          //     <div className="text-white w-[290px] text-center fw-600 syne bg-gradient rounded p-3">
+          //       {data.message}
+          //     </div>
+          //   ),
+          //   position: "top",
+          // });
+        },
+        onError: (err: any) => {
+          toast({
+            title: err.response.data.message,
+            isClosable: true,
+            position: "top",
+            status: "error",
+          });
+        },
+      }
+    );
+  };
+  useEffect(() => {
+    handleFetch();
+  }, []);
   const options = {
     clientSecret: `${data?.data?.clientSecret}`,
   };
+  const [activeTab, setActiveTab] = useState(1);
   return (
-    <div className='py-6 px-2'>
-        {(isPending && !data) && <PaymentFieldShimmer/>}
-         {(!isPending && data) && (
-          <Elements stripe={stripePromise} options={options}>
-            <CheckoutForm id={id} secret_key={data.data?.clientSecret || ""} />
-          </Elements>
-        )}
+    <div className="py-6 px-2">
+      {isPending && !data && <PaymentFieldShimmer />}
+      {!isPending && data && (
+        <>
+          {activeTab === 1 && <BillingAddress next={() => setActiveTab(2)} />}
+          {activeTab === 2 && (
+            <Elements stripe={stripePromise} options={options}>
+              <CheckoutForm
+                id={id}
+                secret_key={data.data?.clientSecret || ""}
+              />
+            </Elements>
+          )}
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default PaymentModal
+export default PaymentModal;
