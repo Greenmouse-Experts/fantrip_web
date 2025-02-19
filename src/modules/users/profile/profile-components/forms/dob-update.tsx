1664a5324fca5dfb01f039a96719eea497ab87d3
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2025-02-19 03:06:11 +0100

    feat: implement calendar date picker for date of birth update and streamline submission process

--- src/modules/users/profile/profile-components/forms/dob-update.tsx
+++ src/modules/users/profile/profile-components/forms/dob-update.tsx
@@ -1,12 +1,16 @@
-import TextInput, { InputType } from "@/components/TextInput";
 import useAuth from "@/hooks/authUser";
 import { updateProfile } from "@/services/api/authApi";
 import { useToast } from "@chakra-ui/react";
 import { useMutation } from "@tanstack/react-query";
 import { FC, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
 import Button from "@/components/Button";
 import { BeatLoader } from "react-spinners";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { FiCalendar } from "react-icons/fi";
+import dayjs from "dayjs";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { getMinimumDate } from "@/lib/utils/formatHelp";
 
 interface Props {
     close: () => void;
@@ -15,28 +19,31 @@ const DobUpdate: FC<Props> = ({ close }) => {
     const toast = useToast();
     const { user, saveUser } = useAuth();
     const [isBusy, setIsBusy] = useState(false);
+    const minimumDate = getMinimumDate(18);
+    const [dateOfBirth, setDate] = useState(user.dob ? new Date(user.dob) : null);
 
-    const {
-        control,
-        handleSubmit,
-        formState: { isValid, errors },
-    } = useForm({
-        mode: "onChange",
-        defaultValues: {
-            dob: user.dob || "",
-        },
-    });
+
+    const handleChange = (val: Date | [Date, Date] | null) => {
+        if (val instanceof Date || val === null) {
+            setDate(val);
+        } else if (Array.isArray(val)) {
+            setDate(val[0]);
+        }
+    };
+
 
     const mutation = useMutation({
         mutationFn: updateProfile,
         mutationKey: ["profileUpdate"],
     });
 
-    const onSubmit = async (datas: any) => {
+    const onSubmit = async () => {
         setIsBusy(true);
+        const formattedDob = dateOfBirth ? dayjs(dateOfBirth).format("YYYY-MM-DD") : "";
+
         const payload = {
             ...user,
-            dob: datas.dob,
+            dob: formattedDob,
         };
         mutation.mutate(payload, {
             onSuccess: (data) => {
@@ -70,39 +77,45 @@ const DobUpdate: FC<Props> = ({ close }) => {
     return (
         <>
             <div className="lg:px-2">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="grid gap-4 grid-cols-1 items-end mt-3">
-                        <Controller
-                            name="dob"
-                            control={control}
-                            rules={{
-                                required: {
-                                    value: true,
-                                    message: "Please enter your date of birth",
-                                },
-                            }}
-                            render={({ field }) => (
-                                <TextInput
-                                    type={InputType.text}
-                                    label=""
-                                    labelClassName="text-black fw-600 lg:text-lg block mb-3"
-                                    borderClass="border border-[#D2D2D2] bg-[#F9FAFC] rounded-[10px] outline-none"
-                                    altClassName="bg-[#F9FAFC] p-3 lg:p-4 rounded-[10px] w-full"
-                                    error={errors.dob?.message}
-                                    {...field}
-                                    ref={null}
+                <div className="grid gap-4 grid-cols-1 items-end mt-3">
+                    <Menu closeOnSelect={false}>
+                        <MenuButton
+                            borderRadius={"xl"}
+                            className="!rounded-[6px] p-3 w-full lg:w-7/12 border border-gray-400"
+                            transition="all 0.2s"
+                        >
+                            <div className="flex gap-x-4 cursor-pointer items-center">
+                                <FiCalendar className="text-xl" />
+                                {dateOfBirth ? (
+                                    <p className="fw-500">
+                                        {dayjs(dateOfBirth as unknown as string).format("DD - MM - YYYY")}
+                                    </p>
+                                ) : (
+                                    <p className="fw-500">PLease select a date</p>
+                                )}
+                            </div>
+                        </MenuButton>
+                        <MenuList className="!pt-0 !pb-0 !rounded-[10px]">
+                            <MenuItem className="rounded-[10px]">
+                                <Calendar
+                                    onChange={(value) => handleChange(value as Date | [Date, Date] | null)}
+                                    value={dateOfBirth}
+                                    maxDate={minimumDate}
+                                    defaultActiveStartDate={minimumDate}
                                 />
-                            )}
-                        />
-                    </div>
+                            </MenuItem>
+                        </MenuList>
+                    </Menu>
 
-                    <div className="mt-7">
-                        <Button
-                            title={isBusy ? <BeatLoader size={12} color="white" /> : "Update"}
-                            disabled={!isValid}
-                        />
-                    </div>
-                </form>
+                </div>
+
+                <div className="mt-7">
+                    <Button
+                        title={isBusy ? <BeatLoader size={12} color="white" /> : "Update"}
+                        disabled={isBusy}
+                        onClick={() => onSubmit()}
+                    />
+                </div>
             </div>
         </>
     );
